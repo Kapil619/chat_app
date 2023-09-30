fce42f61f7f00e723470f68143d71f52e5bd1a0b
kapil619 <kapilbadokar321@gmail.com>
2023-09-30 19:33:13 +0530

    Added buttons on the screen

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -10,6 +10,7 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreen extends State<AuthScreen> {
+  var _isLogin = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,6 +46,27 @@ class _AuthScreen extends State<AuthScreen> {
                           decoration:
                               const InputDecoration(labelText: 'Password'),
                           obscureText: true,
+                        ),
+                        const SizedBox(
+                          height: 12,
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: Theme.of(context)
+                                  .colorScheme
+                                  .primaryContainer),
+                          child: Text(_isLogin ? 'Login' : 'Signup'),
+                        ),
+                        TextButton(
+                          onPressed: () {
+                            setState(() {
+                              _isLogin = !_isLogin;
+                            });
+                          },
+                          child: Text(_isLogin
+                              ? 'Create an account'
+                              : 'I already have an account.'),
                         )
                       ],
                     )),
